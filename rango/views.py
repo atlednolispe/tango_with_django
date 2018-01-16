04c05f02bf2fa4a63ea0189ce8b3e7f1eace0491
atlednolispe <atlednolispe@gmail.com>
2018-01-16 14:55:36 +0800

    Exercises of Django Basics.

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,13 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the rango index.")
+    return HttpResponse("Hello, world. You're at the rango index."
+                        "<br/>"
+                        "<a href='/rango/about'>About</a>")
+
+
+def about(request):
+    return HttpResponse("Rango says here is the about page."
+                        "<br/>"
+                        "<a href='/rango/'>Rango</a>"
+                        )
